0f7ba72422df38492e411611c4d8e9974c0f0038
soumia elyakni <selyakni@gmail.com>
2022-09-22 15:05:10 +0100

    mise en forme du document

--- main.js
+++ main.js
@@ -11,145 +11,148 @@ let cels = [];
 let div = document.createElement("div");
 
 function sira() {
-
-    if(winnerIs == null){
-        turn.innerHTML = "";
-        let div = document.createElement("div");
-        div.textContent = player + "  " + symbole;
-        div.className = "turnOf";
-        turn.append(div);
-    }
-  
+  if (winnerIs == null) {
+    turn.innerHTML = "";
+    let div = document.createElement("div");
+    div.textContent = player + "  " + symbole;
+    div.className = "turnOf";
+    turn.append(div);
+  }
 }
 
 function winner() {
+  for (i = 1; i < 10; i++) {
+    cels[i] = document.getElementById("c" + i);
+  }
 
-    for (i = 1; i < 10; i++) {
-        cels[i] = document.getElementById("c" + i);
-    }
+  //  console.log(cels[1].innerHTML);
+  // console.log(cels);
 
-//  console.log(cels[1].innerHTML);
-    // console.log(cels);
-
-    if (cels[1].innerHTML === cels[2].innerHTML && cels[2].innerHTML === cels[3].innerHTML && cels[1].innerHTML != "") {
-        
-           winnerIs = cels[1].innerHTML;
-           console.log(winnerIs);
-          
-        
-    } else if (cels[4].innerHTML === cels[5].innerHTML && cels[5].innerHTML === cels[6].innerHTML && cels[5].innerHTML != "") {
-        
-           winnerIs = cels[5].innerHTML;
-
-    } else if (cels[7].innerHTML === cels[8].innerHTML && cels[8].innerHTML === cels[9].innerHTML && cels[7].innerHTML != "") {
-
-            winnerIs = cels[7].innerHTML;
-
-    } else if (cels[1].innerHTML === cels[4].innerHTML && cels[4].innerHTML === cels[7].innerHTML && cels[1].innerHTML != "") {
-
-            winnerIs = cels[7].innerHTML;
-
-    } else if (cels[2].innerHTML === cels[5].innerHTML && cels[5].innerHTML === cels[8].innerHTML && cels[5].innerHTML != "") {
-
-            winnerIs = cels[5].innerHTML;
-
-    } else if (cels[3].innerHTML === cels[6].innerHTML && cels[6].innerHTML === cels[9].innerHTML && cels[9].innerHTML != "") {
-
-            winnerIs = cels[9].innerHTML;
-
-    } else if (cels[1].innerHTML === cels[5].innerHTML && cels[5].innerHTML === cels[9].innerHTML && cels[9].innerHTML != "") {
-
-            winnerIs = cels[5].innerHTML;
-
-    } else if (cels[7].innerHTML === cels[3].innerHTML && cels[3].innerHTML === cels[5].innerHTML && cels[7].innerHTML != "") {
-
-            winnerIs = cels[7].innerHTML;
-    }
+  if (
+    cels[1].innerHTML === cels[2].innerHTML &&
+    cels[2].innerHTML === cels[3].innerHTML &&
+    cels[1].innerHTML != ""
+  ) {
+    winnerIs = cels[1].innerHTML;
     console.log(winnerIs);
-    return winnerIs;
+  } else if (
+    cels[4].innerHTML === cels[5].innerHTML &&
+    cels[5].innerHTML === cels[6].innerHTML &&
+    cels[5].innerHTML != ""
+  ) {
+    winnerIs = cels[5].innerHTML;
+  } else if (
+    cels[7].innerHTML === cels[8].innerHTML &&
+    cels[8].innerHTML === cels[9].innerHTML &&
+    cels[7].innerHTML != ""
+  ) {
+    winnerIs = cels[7].innerHTML;
+  } else if (
+    cels[1].innerHTML === cels[4].innerHTML &&
+    cels[4].innerHTML === cels[7].innerHTML &&
+    cels[1].innerHTML != ""
+  ) {
+    winnerIs = cels[7].innerHTML;
+  } else if (
+    cels[2].innerHTML === cels[5].innerHTML &&
+    cels[5].innerHTML === cels[8].innerHTML &&
+    cels[5].innerHTML != ""
+  ) {
+    winnerIs = cels[5].innerHTML;
+  } else if (
+    cels[3].innerHTML === cels[6].innerHTML &&
+    cels[6].innerHTML === cels[9].innerHTML &&
+    cels[9].innerHTML != ""
+  ) {
+    winnerIs = cels[9].innerHTML;
+  } else if (
+    cels[1].innerHTML === cels[5].innerHTML &&
+    cels[5].innerHTML === cels[9].innerHTML &&
+    cels[9].innerHTML != ""
+  ) {
+    winnerIs = cels[5].innerHTML;
+  } else if (
+    cels[7].innerHTML === cels[3].innerHTML &&
+    cels[3].innerHTML === cels[5].innerHTML &&
+    cels[7].innerHTML != ""
+  ) {
+    winnerIs = cels[7].innerHTML;
+  }
+  console.log(winnerIs);
+  return winnerIs;
 }
 
 function start() {
-    if (
-        (gamer1.value == "" && gamer2 == "") ||
-        gamer1.value == "" ||
-        gamer2.value == ""
-    ) {
-        e.preventDefault();
-    } else {
-        localStorage.setItem("G1", gamer1.value);
+  if (
+    (gamer1.value == "" && gamer2 == "") ||
+    gamer1.value == "" ||
+    gamer2.value == ""
+  ) {
+    e.preventDefault();
+  } else {
+    localStorage.setItem("G1", gamer1.value);
 
-        localStorage.setItem("G2", gamer2.value);
+    localStorage.setItem("G2", gamer2.value);
 
-        player = localStorage.getItem("G1");
-    }
+    player = localStorage.getItem("G1");
+  }
 
-    sira();
+  sira();
 }
 
 function fill(id) {
-    if (player != null) {
-        let cel = document.getElementById(id);
-        if (symbole === "X" && cel.innerHTML == "") {
-            cel.innerHTML = "X";
-            symbole = "O";
-            player = localStorage.getItem("G2");
-            
-            winner();
-            
-            if (winnerIs == "X"){
-                var W = window.localStorage.getItem("G1");
-                cel.innerHTML = "X";
-                alert('winner is:'+ W);
-                for (i = 1; i < 10; i++) {
-                    cels[i].innerHTML = "";
-                } 
-                winnerIs=null;
-                
+  if (player != null) {
+    let cel = document.getElementById(id);
+    if (symbole === "X" && cel.innerHTML == "") {
+      cel.innerHTML = "X";
+      symbole = "O";
+      player = localStorage.getItem("G2");
 
-                
-            } else if (winnerIs == "O"){
-                var W = window.localStorage.getItem("G2");
-                cel.innerHTML = "O";
-                alert('winner is:'+ W);
-                for (i = 1; i < 10; i++) {
-                    cels[i].innerHTML = "";
-                }
-                winnerIs=null;
+      winner();
 
-                
-            } 
-            sira();
-            
-        } else if (symbole == "O" && cel.innerHTML == "" ) {
-            cel.innerHTML = "O";
-            symbole = "X";
-            player = localStorage.getItem("G1");
-            sira();
-            winner();
-
-            if (winnerIs == "X"){
-               var W = window.localStorage.getItem("G1");
-               cel.innerHTML = "X";
-                alert('winner is:'+ W);
-                for (i = 1; i < 10; i++) {
-                    cels[i].innerHTML = "";
-                }
-                winnerIs=null;
-                
-            } else if (winnerIs == "O"){
-               var W = window.localStorage.getItem("G2");
-               cel.innerHTML = "O";
-                alert('winner is:'+ W);
-                for (i = 1; i < 10; i++) {
-                    cels[i].innerHTML = "";
-                }
-                winnerIs=null;
-                
-            } 
-            sira();
+      if (winnerIs == "X") {
+        var W = window.localStorage.getItem("G1");
+        cel.innerHTML = "X";
+        alert("winner is:" + W);
+        for (i = 1; i < 10; i++) {
+          cels[i].innerHTML = "";
         }
-        
-        
+        winnerIs = null;
+      } else if (winnerIs == "O") {
+        var W = window.localStorage.getItem("G2");
+        cel.innerHTML = "O";
+        alert("winner is:" + W);
+        for (i = 1; i < 10; i++) {
+          cels[i].innerHTML = "";
+        }
+        winnerIs = null;
+      }
+      sira();
+    } else if (symbole == "O" && cel.innerHTML == "") {
+      cel.innerHTML = "O";
+      symbole = "X";
+      player = localStorage.getItem("G1");
+      sira();
+      winner();
+
+      if (winnerIs == "X") {
+        var W = window.localStorage.getItem("G1");
+        cel.innerHTML = "X";
+        alert("winner is:" + W);
+        for (i = 1; i < 10; i++) {
+          cels[i].innerHTML = "";
+        }
+        winnerIs = null;
+      } else if (winnerIs == "O") {
+        var W = window.localStorage.getItem("G2");
+        cel.innerHTML = "O";
+        alert("winner is:" + W);
+        for (i = 1; i < 10; i++) {
+          cels[i].innerHTML = "";
+        }
+        winnerIs = null;
+      }
+      sira();
     }
+  }
 }
